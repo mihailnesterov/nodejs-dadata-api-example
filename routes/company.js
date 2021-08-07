791a10d25ff65683a1d62f05e35ef8d9d3d10916
mihailnesterov <mhause@mail.ru>
2021-08-08 03:36:12 +0700

    add: lib/utils

--- routes/company.js
+++ routes/company.js
@@ -1,41 +1,15 @@
 const express = require('express');
-const config = require('config');
-const token = config.get('token');
 const router = express.Router();
-const fetch = require('node-fetch');
+const utils = require('../lib/utils');
 
 // get company data by inn
 router.get('/company/:inn', (req, res) => {
 
-    const url       = getCompanyUrl();
-    const options   = getFetchOptions( req.params.inn );
+    const url       = utils.getUrlByName( 'company' );
+    const options   = utils.getOptions( req.params.inn );
     
-    fetchCompanyData(url, options);
+    utils.fetchData(url, options);
 
 });
-
-function getCompanyUrl() {
-    return config.get('url.company.findById');
-}
-
-function getFetchOptions( param ) {
-    return {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": "Token " + token
-        },
-        body: JSON.stringify({query: param})
-    };
-}
-
-function fetchCompanyData(url, options) {
-    fetch(url, options)
-        .then(res => res.json())
-        .then(json => console.log('result', json.suggestions[0].data))
-        .catch(error => console.log("error", error));
-}
 
 module.exports = router;
--- routes/courts.js
+++ routes/courts.js
@@ -1,43 +1,15 @@
 const express = require('express');
-const config = require('config');
-const token = config.get('token');
 const router = express.Router();
-const fetch = require('node-fetch');
+const utils = require('../lib/utils');
 
 // get court data by code
 router.get('/court/:code', (req, res) => {
 
-    const url       = getCourtUrl();
-    const options   = getFetchOptions( req.params.code );
+    const url       = utils.getUrlByName( 'court' );
+    const options   = utils.getOptions( req.params.code );
     
-    fetchCourtData(url, options);
+    utils.fetchData(url, options);
 
 });
-
-
-function getCourtUrl() {
-    return config.get('url.court.findById');
-}
-
-function getFetchOptions( param ) {
-    return {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": "Token " + token
-        },
-        body: JSON.stringify({query: param})
-    };
-    
-}
-
-function fetchCourtData(url, options) {
-    fetch(url, options)
-        .then(res => res.json())
-        .then(json => console.log('result', json.suggestions[0].data))
-        .catch(error => console.log("error", error));
-}
 
 module.exports = router;
